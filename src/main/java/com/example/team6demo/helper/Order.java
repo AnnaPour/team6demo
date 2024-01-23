7a166eb35b38eff532e1707c61e77372a7938bac
AnnaPour <annapourpouri@gmail.com>
2024-01-23 23:52:02 +0200

    23/01/2024

---
+++ src/main/java/com/example/team6demo/helper/Order.java
@@ -0,0 +1,11 @@
+package com.example.team6demo.helper;
+
+package gr.codelearn.spring.showcase.app.mapper;
+
+import gr.codelearn.spring.showcase.app.model.Order;
+import gr.codelearn.spring.showcase.app.transfer.resource.OrderResource;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring", config = IgnoreUnmappedMapperConfig.class)
+public interface OrderMapper extends BaseMapper<Order, OrderResource> {
+}
