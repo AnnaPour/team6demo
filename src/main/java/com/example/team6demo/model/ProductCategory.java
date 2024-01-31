222a9092f61566176883922e586e2c22626b0471
MelinaVasilopoulou <melvas017@gmail.com>
2024-01-31 16:11:12 +0200

    Merge remote-tracking branch 'origin/master'

--- src/main/java/com/example/team6demo/model/ProductCategory.java
+++ src/main/java/com/example/team6demo/model/ProductCategory.java
@@ -1,28 +1,12 @@
 package com.example.team6demo.model;
+import lombok.Getter;
+public class ProductCategory {
 
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
-import lombok.*;
-
-import java.util.List;
-
-@Getter
-@Setter
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-@Entity
-@Table(name = "PRODUCT_CATEGORIES")
-@SequenceGenerator(name = "idGenerator", sequenceName = "PRODUCT_CATEGORIES_SEQ", initialValue = 1, allocationSize = 1)
-public class ProductCategory extends BaseModel {
-
-    @Setter
-    @NotNull
-    @Column(length = 50, nullable = false)
-    private String categoryName;
-    private String description;
-    @OneToMany(mappedBy = "productCategory", cascade = CascadeType.ALL)
-    private List<Product> products;
+    @Getter
+    public enum ProductCategory {
+        COFFEE(), BURGER(), PIZZA(), MEXICAN(), SOUVLAKI();
 
 
+
+    }
 }
