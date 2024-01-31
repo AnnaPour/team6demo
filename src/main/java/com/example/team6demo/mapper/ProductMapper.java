7dc94f81a15048ffb533efe39a45e76b14b597b8
tsirleader <alexia.tsrk@gmail.com>
2024-01-31 14:01:40 +0200

    test 3

--- src/main/java/com/example/team6demo/mapper/ProductMapper.java
+++ src/main/java/com/example/team6demo/mapper/ProductMapper.java
@@ -4,6 +4,7 @@ package com.example.team6demo.mapper;
 
 import com.example.team6demo.model.Product;
 import com.example.team6demo.tran.resource.ProductResource;
+import org.junit.Ignore;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring", config = Ignore.class)
